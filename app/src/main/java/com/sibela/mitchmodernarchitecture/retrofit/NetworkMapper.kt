60ed00fb530ef9fc9f9665e3b82d3ae9b68a8070
leonardosibela <leonardo_sibela@hotmail.com>
2020-08-04 22:46:45 -0300

    Adding NetworkMapper

---
+++ app/src/main/java/com/sibela/mitchmodernarchitecture/retrofit/NetworkMapper.kt
@@ -0,0 +1,32 @@
+package com.sibela.mitchmodernarchitecture.retrofit
+
+import com.sibela.mitchmodernarchitecture.model.Blog
+import com.sibela.mitchmodernarchitecture.util.EntityMapper
+import javax.inject.Inject
+
+class NetworkMapper @Inject constructor() : EntityMapper<BlogNetworkEntity, Blog> {
+
+    override fun mapFromEntity(entity: BlogNetworkEntity): Blog {
+        return Blog(
+            id = entity.id,
+            title = entity.title,
+            body = entity.body,
+            image = entity.image,
+            category = entity.category
+        )
+    }
+
+    override fun mapToEntity(domainModel: Blog): BlogNetworkEntity {
+        return BlogNetworkEntity(
+            id = domainModel.id,
+            title = domainModel.title,
+            body = domainModel.body,
+            image = domainModel.image,
+            category = domainModel.category
+        )
+    }
+
+    fun mapFromEntityList(entities: List<BlogNetworkEntity>): List<Blog> {
+        return entities.map { mapFromEntity(it) }
+    }
+}
