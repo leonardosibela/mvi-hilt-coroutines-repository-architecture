80680720ac37567a2ad03df6403c77f92c10d749
leonardosibela <leonardo_sibela@hotmail.com>
2020-08-09 16:58:47 -0300

    Removing unused imports and organizing code

--- app/src/main/java/com/sibela/mitchmodernarchitecture/di/RoomModel.kt
+++ app/src/main/java/com/sibela/mitchmodernarchitecture/di/RoomModel.kt
@@ -2,17 +2,12 @@ package com.sibela.mitchmodernarchitecture.di
 
 import android.content.Context
 import androidx.room.Room
-import com.google.gson.Gson
-import com.sibela.mitchmodernarchitecture.retrofit.BlogRetrofit
-import com.sibela.mitchmodernarchitecture.room.BlogDao
 import com.sibela.mitchmodernarchitecture.room.BlogDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -22,11 +17,12 @@ object RoomModel {
     @Singleton
     @Provides
     fun provideBlogDb(@ApplicationContext context: Context): BlogDatabase {
-        return Room.databaseBuilder(
-            context,
-            BlogDatabase::class.java,
-            BlogDatabase.DATABASE_NAME
-        )
+        return Room
+            .databaseBuilder(
+                context,
+                BlogDatabase::class.java,
+                BlogDatabase.DATABASE_NAME
+            )
             .fallbackToDestructiveMigration()
             .build()
     }
